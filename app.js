ae45d5801c61fb1c47f094821a032193b3b46c70
muazam-cmd <muazamshahid7@gmail.com>
2021-06-19 06:13:10 -0700

    Mongoose Connected

--- app.js
+++ app.js
@@ -3,10 +3,22 @@ const PORT = 5000;
 const app = express();
 app.use(express.json())
 
+
+const mongoose = require("mongoose")
+mongoose.connect("mongodb+srv://muazamshahid:muazam12345@cluster0.jmq0v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify:false
+})
+mongoose.connection.on("connected", () => {
+    console.log("Mongo Database connected")
+})
+mongoose.connection.on("error", (err) => {
+    console.log("Error connecitng the database", err)
+})
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
-
 app.get("/home", (req, res) => {
   res.send("HOME ROUTE")
 });
