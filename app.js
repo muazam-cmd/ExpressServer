f1df5e3b7fb91e81ab345666d7662f625b18e415
muazam-cmd <muazamshahid7@gmail.com>
2021-06-19 06:18:57 -0700

    Added mongoose schema

--- app.js
+++ app.js
@@ -1,30 +1,34 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const PORT = 5000;
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-
-const mongoose = require("mongoose")
-mongoose.connect("mongodb+srv://muazamshahid:muazam12345@cluster0.jmq0v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+//mongoose connection
+mongoose.connect(
+  "mongodb+srv://muazamshahid:muazam12345@cluster0.jmq0v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  {
     useUnifiedTopology: true,
     useNewUrlParser: true,
-    useFindAndModify:false
-})
+    useFindAndModify: false,
+  }
+);
+// checking the connection
 mongoose.connection.on("connected", () => {
-    console.log("Mongo Database connected")
-})
+  console.log("Mongo Database connected");
+});
 mongoose.connection.on("error", (err) => {
-    console.log("Error connecitng the database", err)
-})
+  console.log("Error connecitng the database", err);
+});
+//api 
+
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
 app.get("/home", (req, res) => {
-  res.send("HOME ROUTE")
+  res.send("HOME ROUTE");
 });
 
-
 app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`)
-})
-
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
